6b6a0650f240c786b4550286a27cc75a9823404d
Anjeet Patel <anjeetpatel0306@gmail.com>
2024-07-16 13:09:10 +0530

    Delete Module 2 directory

--- Module 2/LECTURE 18-CIPHERSCHOOLS/first-app/src/Screens/ToDoScreen.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class ToDoScreen extends Component{
-    render(){
-        return <h1 className="ui heading center">To Do List</h1>
-    }
-}
-
-export default ToDoScreen;
